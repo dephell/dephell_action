7189cbf3ae79762b20322a609227840b370cb171
Gram <master_fess@mail.ru>
2020-05-30 14:46:30 +0200

    show cwd

--- index.js
+++ index.js
@@ -20,12 +20,19 @@ async function run() {
     }
     console.log(`python-version: ${python}`);
     const version = core.getInput('dephell-version');
+    if (version) {
+        console.log(`dephell-version: ${version}`);
+    }
+
+    console.log(process.cwd())
+    fs.readdir(process.cwd(), function (err, items) {
+        console.log(items);
+    })
 
     // download installation script
     const file = fs.createWriteStream(file_name)
     const response = await got(url);
-    file.write(response.body)
-    file.close()
+    file.write(response.body) file.close()
 
     // run installation script
     let options = {
@@ -36,8 +43,7 @@ async function run() {
     if (version) {
         args.push('--version', version)
     }
-    code = await exec.exec('python3', args, options)
-    if (code) {
+    code = await exec.exec('python3', args, options) if (code) {
         core.setFailed("cannot execute installation script")
     }
     fs.unlinkSync(file_name)
@@ -49,22 +55,18 @@ async function run() {
     };
 
     // show dephell info
-    code = await exec.exec('dephell', ['inspect', 'self'], options)
-    if (code) {
+    code = await exec.exec('dephell', ['inspect', 'self'], options) if (code) {
         core.setFailed("cannot run dephell")
     }
 
     // create venv, install dependencies, run the command
-    code = await exec.exec('dephell', ['venv', 'create', '--env', env, '--python', python], options)
-    if (code) {
+    code = await exec.exec('dephell', ['venv', 'create', '--env', env, '--python', python], options) if (code) {
         core.setFailed("cannot create venv")
     }
-    code = await exec.exec('dephell', ['deps', 'install', '--env', env, '--silent'], options)
-    if (code) {
+    code = await exec.exec('dephell', ['deps', 'install', '--env', env, '--silent'], options) if (code) {
         core.setFailed("cannot install deps")
     }
-    code = await exec.exec('dephell', ['venv', 'run', '--env', env], options)
-    if (code) {
+    code = await exec.exec('dephell', ['venv', 'run', '--env', env], options) if (code) {
         core.setFailed("non-zero status code returned by the command")
     }
 }
